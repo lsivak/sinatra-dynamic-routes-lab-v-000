fab122dc3ade0c6fbbdcd7119ebb9998addd9227
Lara Sanders <larasanders@gmail.com>
2017-12-16 15:58:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
 
   get "/say/:number/:phrase" do
-    @say = params[:phrase].repeat do params[:number].to_i
+    @say = params[:phrase]* params[:number].to_i
     "#{@say}"
 end
 get "/say/:word1/:word2/:word3/:word4/:word5" do
